a5c5fa21c9d7902c1bfb9e205207882671aad98c
sagnihotri <sagnihotri@SAGNIHOTRI-N>
2026-02-23 12:55:22 +0530

    Initial TypeScript project

---
+++ 01_intro/intro.js
@@ -0,0 +1,18 @@
+var user = {
+    name: "John",
+    age: 30,
+    isAdmin: true
+};
+console.log(user.name); // John
+// When you run:
+// tsc intro.ts
+// 👉 TypeScript compiles your .ts file into a JavaScript (.js) file.
+// Because:
+// 🔥 Browsers and Node.js understand only JavaScript, NOT TypeScript.
+// So TypeScript converts your code into JavaScript.
+//TypeScript is like:
+// 🛠 A developer tool that produces JavaScript.
+//intro.ts   →   tsc   →   intro.js   →   node intro.js
+//If you don’t want to manually compile every time, you can use:
+// tsc --watch
+// Now TypeScript automatically compiles whenever you save the file.
